bca182ce7cfba581d375e57b51c8f0cbf49aa667
Xeyn19 <edgarrodilorosa@gmail.com>
2025-03-20 12:22:02 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [tailwindcss(), react()],
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(),react()],
   server: {
-    port: 3000,
+    port:3000,
   },
-  base: mode === 'production' ? '/website-portfolio-deploy/' : '/', 
-}))
+  base: process.env.VITE_BASE_PATH || "/website-portfolio-deploy",
+})
