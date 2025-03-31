9ef87a8e4d350572cf2b908aa1808167b3c3bb0d
Xeyn19 <edgarrodilorosa@gmail.com>
2025-03-31 08:31:04 +0800

    build a buttons in projects page that handle filtering date of projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,13 +6,16 @@ import Spinner from '../components/Spinner';
 const Projects = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
+  
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try{
         await new Promise((resolve) => setTimeout(resolve, 1000));
         setData(projectdata)
+        setFilteredData(projectdata)
       } catch (error) {
         console.error('Fetch data error', error)
       } finally {
@@ -23,6 +26,10 @@ const Projects = () => {
 
   }, []);
 
+  const handleRecentProjectsFiltered = (year) => {
+    year === 'all' ? setFilteredData(data) : setFilteredData(data.filter((project) => project.date === year));
+  }
+
   if(loading){
     return <Spinner /> 
   }
@@ -31,10 +38,25 @@ const Projects = () => {
     <div className="flex min-h-screen">
       <div className="w-full h-full px-40 py-20 max-md:px-5 max-xl:p-0 max-xl:text-center">
         <h1 className="font-bold text-4xl tracking-wider block">Projects</h1>
-        {data.map((project) => (
-          <div key={project.id} className="flex space-x-30 mt-10 mb-40 max-md:flex-col max-md:mt-4 max-md:mb-10 max-xl:flex-col">
+        <div className="button-projects flex gap-3 my-6 max-md:grid max-md:grid-cols-2 max-md:px-20">
+          <button className='py-2 px-3 bg-yellow-600 font-medium rounded-md hover:bg-yellow-700 transition-all duration-300 ease-in-out
+          text-white max-md:text-[12px] max-md:py-1 max-md:px-2 cursor-pointer' 
+            onClick={() => handleRecentProjectsFiltered(2025)}>Recent Projects (2025)</button>
+          <button className='py-2 px-3 bg-yellow-600 font-medium rounded-md hover:bg-yellow-700 transition-all duration-300 ease-in-out
+          text-white max-md:text-[12px] max-md:py-1 max-md:px-2 cursor-pointer'
+            onClick={() => handleRecentProjectsFiltered(2024)}>Previous Projects (2024)</button>
+          <button className='py-2 px-3 bg-yellow-600 font-medium rounded-md hover:bg-yellow-700 transition-all duration-300 ease-in-out
+          text-white max-md:text-[12px] max-md:py-1 max-md:px-2 cursor-pointer'
+            onClick={() => handleRecentProjectsFiltered(2023)}>Previous Projects (2023)</button>
+          <button className='py-2 px-3 bg-yellow-600 font-medium rounded-md hover:bg-yellow-700 transition-all duration-300 ease-in-out
+          text-white max-md:text-[12px] max-md:py-1 max-md:px-2 cursor-pointer'
+            onClick={() => handleRecentProjectsFiltered('all')}>All Projects</button>
+        </div>
+        {filteredData.length > 0 ? (
+          filteredData.map((project) => (
+            <div key={project.id} className="flex space-x-30 mt-10 mb-40 max-md:flex-col max-md:mt-4 max-md:mb-10 max-xl:flex-col">
       
-            <div className="w-[50%] space-y-4 max-md:w-[100%] self-center max-md:self-start max-xl:w[100%] max-xl:m-auto">
+            <div className="w-[50%] space-y-5 max-md:w-[100%] self-center max-md:self-start max-xl:w[100%] max-xl:m-auto max-md:pt-5" >
               <h2 className="font-bold text-lg max-xl:text-xl">{project.title}</h2>
               <p className="text-gray-600 text-sm max-xl:text-md">{project.description}</p>
            
@@ -46,11 +68,14 @@ const Projects = () => {
               </div>
             </div>
 
-            <div className="w-[50%] max-md:w-[100%] max-md:mt-10 max-xl:w[100%] max-xl:m-auto"> 
+            <div className="w-[50%] max-md:w-[100%] max-md:mt-10 max-xl:w[100%] max-xl:m-auto max-md:mb-15"> 
               <img src={project.image} alt={project.title} className="w-[500px] rounded-lg shadow-sm" />
             </div>
           </div>
-        ))}
+          ))
+        ) : (
+          <p className="text-gray-600 text-center mt-5">No projects found for the selected year.</p>
+        )}
       </div>
     </div>
   );
