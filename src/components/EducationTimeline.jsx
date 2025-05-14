2adb9044fdfb34e7962f19e94101ae44c33f3ae9
Edgar Jr <edgarrodilorosa@gmail.com>
2025-05-14 17:14:44 +0800

    fix5

--- src/components/EducationTimeline.jsx
+++ src/components/EducationTimeline.jsx
@@ -4,22 +4,22 @@ const EducationTimeline = () => {
   return (
     <ul className="timeline timeline-vertical text-sm text-slate-800">
   <li>
-    <div className="timeline-start timeline-box"> <span className='font-medium'>Pulanlupa Elementary School.</span> <br /> (2010-2016)</div>
+    <div className="timeline-start timeline-box bg-slate-100"> <span className='font-medium'>Pulanlupa Elementary School.</span> <br /> (2010-2016)</div>
     <hr />
   </li>
   <li>
     <hr />
-    <div className="timeline-end timeline-box"> <span className='font-medium'>Las Pinas North National High School (LPNNHS).</span> (2016-2020)</div>
+    <div className="timeline-end timeline-box bg-slate-100"> <span className='font-medium'>Las Pinas North National High School (LPNNHS).</span> (2016-2020)</div>
     <hr />
   </li>
   <li>
     <hr />
-    <div className="timeline-start timeline-box"> <span className='font-medium'>Holy Rosary Academy of Laspinas City.</span> (2020-2022)</div>
+    <div className="timeline-start timeline-box bg-slate-100"> <span className='font-medium'>Holy Rosary Academy of Laspinas City.</span> (2020-2022)</div>
     <hr />
   </li>
   <li>
     <hr />
-    <div className="timeline-end timeline-box"> <span className='font-medium'>Dr.Filemon C. Aguilar Memorial College IT-Campus.</span> (2022-2026)</div>
+    <div className="timeline-end timeline-box bg-slate-100"> <span className='font-medium'>Dr.Filemon C. Aguilar Memorial College IT-Campus.</span> (2022-2026)</div>
     <hr />
   </li>
 </ul>
