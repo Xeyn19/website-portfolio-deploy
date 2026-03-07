3dcfa712d2beb0c4106d7ecd0bd6d70d87a46a54
Edgar Orosa <edgarrodilorosa@gmail.com>
2026-03-07 14:53:47 +0800

    dark mode default theme

--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext();
 const ThemeProvider = ({children}) => {
-    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+    const [theme, setTheme] = useState('dark');
 
     useEffect(() => {
         localStorage.setItem('theme', theme);
